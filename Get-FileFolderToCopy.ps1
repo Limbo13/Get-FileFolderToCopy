299135300337a7ac852b6ebca941d38bb14df4f1
Derrick Michelson <derrick.michelson@wwu.edu>
2018-10-17 16:32:16 -0700

    Added help and modified a few portions of the code.  Version 1.0 is complete

--- Get-FileFolderToCopy.ps1
+++ Get-FileFolderToCopy.ps1
@@ -1,29 +1,58 @@
-#Based this script off of this article: https://workingsysadmin.com/open-file-dialog-box-in-powershell/
-Function Get-FileFolderToCopy($FileOrFolder)
+<#
+    .SYNOPSIS
+    This script lets you choose a list of files or folders from a GUI interface.
+
+    .DESCRIPTION
+    This script lets you choose a list of files or folders from a GUI interface.  Variable takes "file" or "folder".
+
+    When the command is run, a selection window appears letting you choose files or folders, depending on what you specify when calling the script.
+
+    If you choose files, you can select multiple files at once from the same folder.  If you choose folders, you can select one, click OK, then choose another.
+    This will repeat until Cancel is clicked.
+
+    An array named $SelectedList is returned with the results.
+
+    .EXAMPLE
+    Get-FileFolderToCopy("File")
+
+    .EXAMPLE
+    Get-FileFolderToCopy("Folder")
+
+    .EXAMPLE
+    Get-FileFolderToCopy("Files")
+#>
+Function Get-FileFolderToCopy()
 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$FileOrFolder
+        )
+
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
-    If ($FileOrFolder -eq "File")
+    If ($FileOrFolder -like "File*")
     {
         $OpenSelector = New-Object System.Windows.Forms.OpenFileDialog
         $OpenSelector.InitialDirectory = $initialDirectory
+        $OpenSelector.Multiselect = $true
         $OpenSelector.ShowDialog() | Out-Null
-        Return $OpenSelector.FileName
+        $global:SelectedList = $OpenSelector.FileNames
     }
-    elseif ($FileOrFolder -eq "Folder")
+    elseif ($FileOrFolder -like "Folder*")
     {
         $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
         $SelectionsDone = $false
+        $global:SelectedList = @()
 
         Do{
             If ($OpenFolderDialog.ShowDialog() -eq "OK")
             {
-                $OpenFolderDialog.SelectedPath
+                $global:SelectedList += $OpenFolderDialog.SelectedPath
             }
             else
             {
                 $SelectionsDone = $true
-                Write-Output "Cancel"
             }
         } While ($SelectionsDone -eq $false)
     }

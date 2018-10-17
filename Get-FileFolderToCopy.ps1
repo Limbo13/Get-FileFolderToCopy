2647f5081653265224aeffc823a1c432e57f8dc2
Derrick Michelson <derrick.michelson@wwu.edu>
2018-10-17 11:12:21 -0700

    Added basic logic for choosing files or folders, and the code to allow the selection via GUI

--- Get-FileFolderToCopy.ps1
+++ Get-FileFolderToCopy.ps1
@@ -1,8 +1,34 @@
-Function Get-FileFolderToCopy()
+#Based this script off of this article: https://workingsysadmin.com/open-file-dialog-box-in-powershell/
+Function Get-FileFolderToCopy($FileOrFolder)
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
-    $OpenSelector = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenSelector.ShowDialog() | Out-Null
-    $OpenSelector.FileName
+    If ($FileOrFolder -eq "File")
+    {
+        $OpenSelector = New-Object System.Windows.Forms.OpenFileDialog
+        $OpenSelector.InitialDirectory = $initialDirectory
+        $OpenSelector.ShowDialog() | Out-Null
+        Return $OpenSelector.FileName
+    }
+    elseif ($FileOrFolder -eq "Folder")
+    {
+        $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+        $SelectionsDone = $false
+
+        Do{
+            If ($OpenFolderDialog.ShowDialog() -eq "OK")
+            {
+                $OpenFolderDialog.SelectedPath
+            }
+            else
+            {
+                $SelectionsDone = $true
+                Write-Output "Cancel"
+            }
+        } While ($SelectionsDone -eq $false)
+    }
+    else
+    {
+        Write-Error "Please specify File or Folder"
+    }
 }
